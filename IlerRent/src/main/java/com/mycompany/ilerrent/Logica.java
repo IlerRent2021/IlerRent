6b9f2613e9f69b3e796e693bf29c2816b059e989
alumno <alumno@PC-S12816.mshome.net>
2021-12-20 20:35:30 +0100

    inicio y pefil de usuario

---
+++ IlerRent/src/main/java/com/mycompany/ilerrent/Logica.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ilerrent;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+/**
+ *
+ * @author alumno
+ */
+public class Logica {
+   
+    public void timer(){
+        final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        final Runnable runnable = new Runnable() {
+            int countdownStarter = 10;
+            
+            @Override
+            public void run(){
+                countdownStarter--;
+                
+                if (countdownStarter < 0){
+            
+                System.exit(0);
+            
+                }
+            }
+         
+        };
+         scheduler.scheduleAtFixedRate(runnable, 0, 5, SECONDS);
+    }
+    
+}
