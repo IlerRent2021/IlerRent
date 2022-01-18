8d3eae9c7e6970db33203818d6075a7ded1525ef
Jose Luis <jolugara2000@gmail.com>
2022-01-18 20:45:28 +0100

    Añadimos el timer y un posible solución futura

--- IlerRent/src/main/java/Logica/Logica.java
+++ IlerRent/src/main/java/Logica/Logica.java
@@ -43,4 +43,50 @@ public class Logica {
 //        }
 //        panelCoches.updateUI();
     }
+    //Cronometro para resetear los datos metidos al pasar cierto tiempo
+        public void timer(){
+        final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        final Runnable runnable = new Runnable() {
+            int countdownStarter = 10;
+            
+            @Override
+            public void run(){
+                countdownStarter--;
+                
+                if (countdownStarter < 0){
+            
+                System.exit(0);
+            
+                }
+            }
+         
+        };
+         scheduler.scheduleAtFixedRate(runnable, 0, 5, SECONDS);
+    }
+    //Posible solucion a mis errores no borrar porfa
+//    public void CargarImagen(){
+//        Blob blob = resultset.getBlob(3);
+//        //primero me aseguro que no este vacío.
+//        if(blob != null){
+//        try{
+//            byte[] data = blob.getBytes(1, (int)blob.length());
+//            BufferedImage img = null;
+//
+//        try{
+//            img = ImageIO.read(new ByteArrayInputStream(data));
+//        }
+//        catch(Exception ex){
+//            System.out.println(ex.getMessage());
+//        }
+//
+//        ImageIcon icono = new ImageIcon(img);
+//        JOptionPane.showMessageDialog(null, "Imagenes", "Imagen", JOptionPane.INFORMATION_MESSAGE, icono);
+//
+//        }catch(Exception ex){
+//        //No hay imagen
+//        }
+//    }else{
+//        //No hay imagen
+//    }
+//   }
 }
