69c0edbda34b9a835d25cfc85378c40e7ef54320
alumno <alumno@PC-S12813.ilerna.sevilla>
2022-01-31 20:55:23 +0100

    proyecto felipe-->casi terminado la creacion de la reserva :)

--- IlerRent/src/main/java/vistas/panelCoches.java
+++ IlerRent/src/main/java/vistas/panelCoches.java
@@ -130,22 +130,28 @@ public class panelCoches extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jCheckBox1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jCheckBox1MouseClicked
-        if (jCheckBox1.isSelected()==true) {
+        
+        
+
+    }//GEN-LAST:event_jCheckBox1MouseClicked
+
+    private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
+         if (jCheckBox1.isSelected()==true) {
             
             flagHabilita=true;
             flagHabilitatodos=false;
-        } else {
+        } else if(!jCheckBox1.isEnabled()){
+            flagHabilita=false;
+            flagHabilitatodos=false;
+        }else{
 
             flagHabilita=false;
             flagHabilitatodos=true;
             
         }
         logica.habilita(panelLista);
+    
 
-    }//GEN-LAST:event_jCheckBox1MouseClicked
-
-    private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
-        // TODO add your handling code here:
     }//GEN-LAST:event_jCheckBox1ActionPerformed
 
     public JCheckBox getjCheckBox1() {
